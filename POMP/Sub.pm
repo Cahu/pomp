af539c975d19d929bec64506476f0f252f9725f3
cahu <cahu@gmx.us>
2013-12-12 10:10:02 +0100

    POMP::Sub module

---
+++ POMP/Sub.pm
@@ -0,0 +1,28 @@
+package POMP::Sub;
+
+use strict;
+use warnings;
+
+
+sub new {
+	my ($class, $sub_name, $args, $code) = @_;
+	return bless {
+		name => $sub_name,
+		args => $args,
+		code => $code,
+	}, $class;
+}
+
+
+sub print {
+	my ($self, $out) = @_;
+
+	if (!defined $out) {
+		$out = \*STDOUT;
+	}
+
+	print $out "sub " . $self->{name};
+	print $out $self->{code};
+}
+
+1;

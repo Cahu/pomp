f3d6d2b4e090633ec08df038086bf0cce1bcf3d4
cahu <cahu@gmx.us>
2013-12-12 09:18:35 +0100

    start of thread management code

---
+++ threads.pl
@@ -0,0 +1,55 @@
+#use strict;
+#use warnings;
+
+use threads;
+use Thread::Queue;
+
+my @POMP_QUEUES;
+my @POMP_THREADS;
+
+use constant {
+	CALL => 1,
+	EXIT => 2,
+};
+
+
+sub truc { print "Hello World!\n"; }
+$_->enqueue([CALL, "truc"]) for (@POMP_QUEUES);
+
+
+BEGIN {
+	# consumer function
+	sub POMP_CONSUMER {
+		my $queue = shift;
+		my $self  = threads->self();
+		print "Thread " . $self->tid() . " started.\n";
+
+		while (my $instr = $queue->dequeue()) {
+			my ($code, @args) = @$instr;
+
+			if ($code == CALL) {
+				print "Thread " . $self->tid() . " runs a job.\n";
+				my ($sub, @sub_args) = @args;
+				{
+					no strict 'refs';
+					$sub->(@sub_args);
+				}
+			} elsif ($code == EXIT) {
+				last;
+			}
+		}
+
+		print "Thread " . $self->tid() . " stoped.\n";
+	}
+
+	@POMP_QUEUES  = map { Thread::Queue->new(); } (0..3);
+	@POMP_THREADS = map {
+		threads->create(\&POMP_CONSUMER, $POMP_QUEUES[$_]);
+	} (0..3);
+}
+
+END {
+	# clean up
+	$_->enqueue([EXIT, undef]) for (@POMP_QUEUES);
+	$_->join() for (@POMP_THREADS);
+}

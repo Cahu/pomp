7d08e55101fb2eff27b263b98875eca4c314f84d
cahu <cahu@gmx.us>
2013-12-22 13:44:30 +0100

    Indenting tools

--- POMP/Indent.pm
+++ POMP/Indent.pm
@@ -1,11 +1,42 @@
 package POMP::Indent;
 
+
+sub indent {
+	my ($code) = @_;
+	$code =~ s/^/\t/gm;
+	return $code;
+}
+
+
 sub reindent {
 	my ($code) = @_;
 
 	if ($code =~ /^(\t*)/) {
 		$code =~ s/^$1//gme;
 	}
+
+	return $code;
+}
+
+
+sub get_level {
+	my ($code) = @_;
+
+	if ($code =~ /^(\t*)/) {
+		return length $1;
+	}
+
+	return 0;
+}
+
+
+sub set_level {
+	my ($code, $lvl) = @_;
+
+	$lvl //= 1;
+
+	my $indent = "\t" x $lvl;
+	$code =~ s/^\t*/$indent/gm;
 
 	return $code;
 }

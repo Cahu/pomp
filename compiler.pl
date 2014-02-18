3cad3a20932703a2fff1539c3889fa03bf878cfe
cahu <cahu@gmx.us>
2014-02-18 20:10:21 +0100

    compiler.pl options and help

--- compiler.pl
+++ compiler.pl
@@ -2,18 +2,63 @@ use strict;
 use warnings;
 
 use Parser;
+use Getopt::Long;
 
-usage() unless (defined $ARGV[0]);
+my $usage = <<EOU;
+/path/to/perl $0 [options] <source file>
 
-open(my $file, "<", $ARGV[0]) or die "Can't open $ARGV[0]: $!";
+Options:
+	-o <output file>     specify output file
+	-h                   show this help
+EOU
 
+
+# Handle command line options
+my $show_help;
+my $output_file = "out.pl";
+GetOptions(
+	"o=s"  => \$output_file,
+	"h"    => \$show_help,
+	"help" => \$show_help,
+);
+
+# Check whether an input file was given
+if ($show_help || !defined $ARGV[0]) {
+	print $usage;
+	exit;
+}
+
+# Open output file for writing
+my $out;
+if ($output_file eq "stdout") {
+	open($out, ">&", STDOUT)
+		or die "Can't dup STDOUT: $!";
+} elsif ($output_file eq "stderr") {
+	open($out, ">&", STDERR)
+		or die "Can't dup STDERR $!";
+} else {
+	open($out, ">", $output_file)
+		or die "Can't open $output_file: $!";
+}
+
+# Open input file
+open(my $file, "<", $ARGV[0])
+	or die "Can't open $ARGV[0]: $!";
+
+
+# Slurp
 my $p = Parser->new();
-$p->YYData->{DATA} = do {local $/, <$file>};
+$p->YYData->{DATA} = do {
+	local $/;
+	<$file>;
+};
+
 
 close($file);
 
 
-print <<'EOP';
+# header
+print $out <<'EOP';
 ### POMP Header ---------------------------------------------
 use POMP;
 use threads::shared;          # for shared vars
@@ -22,24 +67,27 @@ use Storable qw(freeze thaw); # for copying firstprivate vars
 
 EOP
 
-$p->YYParse(YYlex => \&Parser::lex, YYerror => \&Parser::error);
 
-print <<'EOP';
+# modified user code
+select $out; # write everything to the output file
+$p->YYParse(YYlex => \&Parser::lex, YYerror => \&Parser::error);
+select STDOUT;
+
+
+# footer (generated subs)
+print $out <<'EOP';
 
 ### POMP GENERATED SUBS -------------------------------------
 
 EOP
 
 foreach my $s (@Parser::POMP_subs) {
-	print $s->gen_body;
-	print "\n";
+	print $out $s->gen_body;
+	print $out "\n";
 }
 
-print <<'EOP';
+print $out <<'EOP';
 ### ---------------------------------------------------------
 EOP
 
-
-sub usage {
-	die "Usage: ...";
-}
+close($out);

b05e2f4a333d1e77f4242278f76e17cfcfaccac8
cahu <cahu@gmx.us>
2014-02-19 12:09:16 +0100

    fix run_tests.pl

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,7 @@
 
 for i in $(ls samples/); do
 	echo "== Test: $i =========================";
-	perl -Mblib compiler.pl samples/$i > out.pl
+	perl -Mblib compiler.pl -o out.pl samples/$i
 	perl -Mblib out.pl
 	rm out.pl
 done

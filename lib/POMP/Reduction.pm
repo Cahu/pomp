9ea14fa744fb40314fb8dd2a099f5a8a376b4963
cahu <cahu@gmx.us>
2014-03-01 16:59:20 +0100

    Reduction.pm documentation

--- lib/POMP/Reduction.pm
+++ lib/POMP/Reduction.pm
@@ -27,3 +27,31 @@ sub apply {
 
 1;
 
+__END__
+
+=pod
+
+=head1 Methods
+
+=over 4
+
+=item C<new($init_value, $sub)>
+
+The scalar passed in C<$init_value> is the value to use to initialize the accumulator.
+
+The subroutine passed via C<$sub> shall take two arguments. The first one is the
+accumulator, the second one is the name of a reference to a list of values to be
+prosessed and adeded to the accumulator.
+
+=item C<init>
+
+Returns the init value of the accumulator.
+
+=item C<apply>
+
+Output the generated code.
+
+=back
+
+=cut
+
